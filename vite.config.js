367f4ba10e41ecebc29f47fecb0531c27e37a62b
meowlyricsreal <meowlyrics1@gmail.com>
2026-05-26 10:56:51 +0800

    update website

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/pri-website/',
+
+  server: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 3000
+  },
+
+  preview: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 3000
+  }
 })
