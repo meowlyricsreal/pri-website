f944fc6112af904da94ea0d4bd71874c2081618e
meowlyricsreal <meowlyrics1@gmail.com>
2026-05-28 21:15:16 +0800

    update website

--- vite.config.js
+++ vite.config.js
@@ -4,16 +4,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/pri-website/',
-
-  server: {
-    host: '0.0.0.0',
-    port: process.env.PORT || 3000,
-    allowedHosts: ['ofiicial-web-pri.onrender.com']
-  },
-
-  preview: {
-    host: '0.0.0.0',
-    port: process.env.PORT || 3000,
-    allowedHosts: ['ofiicial-web-pri.onrender.com']
-  }
 })
